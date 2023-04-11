6467efad7b4f3a6a8c086e84f56217ee0ab53c4e
Volsavr <volodymyr.babii@bluejeans.com>
2023-04-11 10:18:11 +0300

    sample app for using path data resource in winui3

---
+++ src/PathTestApp/PathTestApp/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Xaml.Media;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace PathTestApp
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        private SolidColorBrush _redBrush = new SolidColorBrush(Colors.Red);
+        private SolidColorBrush _blackBrush = new SolidColorBrush(Colors.Black);
+        private SolidColorBrush _currentBrush;
+        private bool _state = true;
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            _currentBrush = _blackBrush;
+            ChangeColorCommand = new RelayCommand((obj) =>
+            {
+                var brush = _state? _redBrush : _blackBrush;
+                this.CurrentBrush = brush;
+                _state = !_state;
+            });
+        }
+        #endregion
+
+        #region Properties
+        public SolidColorBrush CurrentBrush
+        {
+            get
+            {
+                return _currentBrush;
+            }
+            set
+            {
+                _currentBrush = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(CurrentBrush)));
+            }
+        } 
+
+        public ICommand ChangeColorCommand { get; private set; }
+        #endregion
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged; 
+        #endregion
+    }
+}
---
+++ src/PathTestApp/PathTestApp/PathDataConverter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Markup;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace PathTestApp
+{
+    public class PathDataConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string pathData = value as string;
+            if (null != pathData)
+                return (Geometry)XamlBindingHelper.ConvertValue(typeof(Geometry), pathData); 
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
